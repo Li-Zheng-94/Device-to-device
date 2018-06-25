6aa2315f93d793fc044e6a399eebccb58387bb5d
ZacharyLeebupt <lizhengzachary@outlook.com>
2018-06-25 21:57:22 +0800

    设备类

--- device.py
+++ device.py
@@ -12,33 +12,6 @@ class Interface(object):
         return self.__id
 
 
-# 用户类
-class User(Interface):
-    def __init__(self, i_id):
-        # 调用父类的构造函数
-        Interface.__init__(self, i_id)
-
-        self.__x_point = -1
-        self.__y_point = -1
-        self.__allocated_RB = []
-
-    def set_location(self, x_point, y_point):
-        self.__x_point = x_point
-        self.__y_point = y_point
-
-    def get_x_point(self):
-        return self.__x_point
-
-    def get_y_point(self):
-        return self.__y_point
-
-    def set_allocated_rb(self, rb_id):
-        self.__allocated_RB.append(rb_id)
-
-    def get_allocated_rb(self):
-        return self.__allocated_RB
-
-
 # 基站类
 class BS(Interface):
     def __init__(self, i_id):
@@ -47,7 +20,7 @@ class BS(Interface):
         self.__power = 20  # 发射功率 dBm
         self.__x_point = 0
         self.__y_point = 0
-        self.__allocated_RB = []
+        self.__allocated_rb = []
 
     def set_location(self, x_point, y_point):
         self.__x_point = x_point
@@ -63,8 +36,62 @@ class BS(Interface):
         return self.__power
 
     def set_allocated_rb(self, rb_id):
-        self.__allocated_RB.append(rb_id)
+        self.__allocated_rb.append(rb_id)
 
     def get_allocated_rb(self):
-        return self.__allocated_RB
+        return self.__allocated_rb
 
+
+# 用户类
+class User(Interface):
+    def __init__(self, i_id):
+        # 调用父类的构造函数
+        Interface.__init__(self, i_id)
+
+        self.__x_point = -1
+        self.__y_point = -1
+        self.__allocated_rb = []
+
+    def set_location(self, x_point, y_point):
+        self.__x_point = x_point
+        self.__y_point = y_point
+
+    def get_x_point(self):
+        return self.__x_point
+
+    def get_y_point(self):
+        return self.__y_point
+
+    def set_allocated_rb(self, rb_id):
+        self.__allocated_rb.append(rb_id)
+
+    def get_allocated_rb(self):
+        return self.__allocated_rb
+
+
+# 蜂窝用户类
+class CUE(User):
+    def __init__(self, i_id):
+        User.__init__(self, i_id)
+
+
+# D2D发射机类
+class D2DTx(User):
+    def __init__(self, i_id):
+        User.__init__(self, i_id)
+        self.__rx_id = -1
+
+    # 配对
+    def make_pair(self, rx_id):
+        self.__rx_id = rx_id
+
+
+# D2D接收机类
+class D2DRx(User):
+    def __init__(self, i_id):
+        User.__init__(self, i_id)
+        self.__tx_id = -1
+
+    # 配对
+    def make_pair(self, tx_id):
+        self.__tx_id = tx_id
